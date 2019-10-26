a897fb7dc490f1de94cf5b9607307d0841a3df06
nika <pinskaya.nika@mail.ru>
2019-10-26 22:29:22 +0300

    Scripted auto-commit on change (2019-10-26 22:29:22) by gitwatch.sh

--- lab_3/src/main/java/com/examples/sparkflight/SparkFlight.java
+++ lab_3/src/main/java/com/examples/sparkflight/SparkFlight.java
@@ -1,4 +1,13 @@
 package com.examples.sparkflight;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class SparkFlight {
+    public static void main(String[] args) throws Exception {
+        SparkConf conf = new SparkConf().setAppName("Lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        
+    }
 }
